ed6a0383ed9d089bd81d2f45d13cb50863b7f686
arokem <arokem@gmail.com>
2016-11-29 15:53:10 -0800

    Convert to PDF.

---
+++ pdfify.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+kramdown _episodes/01-welcome.md --no-auto-ids --output latex > tex/01-welcome.tex
+kramdown _episodes/02-novice.md --no-auto-ids --output latex > tex/02-novice.tex
+kramdown _episodes/03-terms.md --no-auto-ids --output latex > tex/03-terms.tex
+kramdown _episodes/04-coffee.md --no-auto-ids --output latex > tex/04-coffee.tex
+kramdown _episodes/05-performance.md --no-auto-ids --output latex > tex/05-performance.tex
+kramdown _episodes/06-summarize.md --no-auto-ids --output latex > tex/06-summarize.tex
+kramdown _episodes/07-lunch.md --no-auto-ids --output latex > tex/07-lunch.tex
+kramdown _episodes/08-memory.md --no-auto-ids --output latex > tex/08-memory.tex
+kramdown _episodes/09-coffee.md --no-auto-ids --output latex > tex/09-coffee.tex
+kramdown _episodes/10-performance.md --no-auto-ids --output latex > tex/10-performance.tex
+kramdown _episodes/11-load.md --no-auto-ids --output latex > tex/11-load.tex
+kramdown _episodes/12-summarize.md --no-auto-ids --output latex > tex/12-summarize.tex
+kramdown _episodes/13-live.md --no-auto-ids --output latex > tex/13-live.tex
+kramdown _episodes/14-coffee.md --no-auto-ids --output latex > tex/14-coffee.tex
+kramdown _episodes/15-practices.md --no-auto-ids --output latex > tex/15-practices.tex
+kramdown _episodes/16-motivation.md --no-auto-ids --output latex > tex/16-motivation.tex
+kramdown _episodes/17-summarize.md --no-auto-ids --output latex > tex/17-summarize.tex
+kramdown _episodes/18-lunch.md --no-auto-ids --output latex > tex/18-lunch.tex
+kramdown _episodes/19-lessons.md --no-auto-ids --output latex > tex/19-lessons.tex
+kramdown _episodes/20-coffee.md --no-auto-ids --output latex > tex/20-coffee.tex
+kramdown _episodes/21-carpentries.md --no-auto-ids --output latex > tex/21-carpentries.tex
+kramdown _episodes/22-summarize.md --no-auto-ids --output latex > tex/22-summarize.tex
